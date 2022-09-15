2ac3cb4838e9519895933d8053e99e4da621abd1
Michael Pöhn <michael@poehn.at>
2022-09-15 02:36:04 +0200

    ci for runing cache scripts and deploying to gitlab pages

--- tools/buildcache.py
+++ tools/buildcache.py
@@ -7,13 +7,22 @@ import json
 import yaml
 
 
+SIG_SOURCES = {
+    'izzy.json': Path('izzy'),
+    'sigs.json': Path('sigs'),
+}
+
+
 def parseyml(filepath):
     with open(filepath, 'r', encoding='utf8') as f:
         return yaml.safe_load(f)
 
 
 if __name__ == '__main__':
-    for cachefilename, sigdir in [('izzy.json', Path('izzy')), ('sigs.json', Path('sigs'))]:
+    outdir = Path('public')
+    os.makedirs(outdir, exist_ok=True)
+
+    for cachefilename, sigdir in SIG_SOURCES.items():
         print('assembling', cachefilename, '...', end=' ')
         cache = {
             'timestamp': datetime.now().astimezone().isoformat(),
@@ -22,6 +31,9 @@ if __name__ == '__main__':
         }
         for name, data in [(x, parseyml(sigdir / x)) for x in os.listdir(sigdir) if x.endswith('.yml')]:
             cache['signatures'][name[:-4]] = data
-        with open(cachefilename, 'w', encoding='utf8') as f:
+        with open(outdir / cachefilename, 'w', encoding='utf8') as f:
             json.dump(cache, f, sort_keys=True, indent=2)
         print(len(cache['signatures']), 'entries ... OK')
+
+    with open(outdir / 'index.html', 'w', encoding='utf8') as f:
+        f.write('<html><body><ul>{}</ul></body></html>'.format(''.join(['<li><a href="{}">{}</a></li>'.format(x, x) for x in SIG_SOURCES.keys()])))
