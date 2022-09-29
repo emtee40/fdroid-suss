14076524f96c540666978cfa35ba4a8d292acc44
Michael Pöhn <michael@poehn.at>
2022-09-29 21:32:21 +0200

    add config.yml and cache_duration setting

--- tools/buildcache.py
+++ tools/buildcache.py
@@ -13,6 +13,9 @@ SIG_SOURCES = {
 }
 
 
+CONFIG_DEFAULTS = {'cache_duration': 86400}
+
+
 def parseyml(filepath):
     with open(filepath, 'r', encoding='utf8') as f:
         return yaml.safe_load(f)
@@ -22,19 +25,34 @@ if __name__ == '__main__':
     outdir = Path('public')
     outdir.mkdir(exist_ok=True)
 
+    config = dict(CONFIG_DEFAULTS)
+    config.update(parseyml('config.yml'))
+
     for cachefilename, sigdir in SIG_SOURCES.items():
         print('assembling', cachefilename, '...', end=' ')
         cache = {
             'timestamp': datetime.now().astimezone().isoformat(),
             'version': 1,
-            'signatures': {}
+            'signatures': {},
+            'cache_duration': config['cache_duration'],
         }
         sigdir.mkdir(exist_ok=True)
-        for name, data in [(x, parseyml(sigdir / x)) for x in os.listdir(sigdir) if x.endswith('.yml')]:
+        for name, data in [
+            (x, parseyml(sigdir / x)) for x in os.listdir(sigdir) if x.endswith('.yml')
+        ]:
             cache['signatures'][name[:-4]] = data
         with open(outdir / cachefilename, 'w', encoding='utf8') as f:
             json.dump(cache, f, sort_keys=True, indent=2)
         print(len(cache['signatures']), 'entries ... OK')
 
     with open(outdir / 'index.html', 'w', encoding='utf8') as f:
-        f.write('<html><body><ul>{}</ul></body></html>'.format(''.join(['<li><a href="{}">{}</a></li>'.format(x, x) for x in SIG_SOURCES.keys()])))
+        f.write(
+            '<html><body><ul>{}</ul></body></html>'.format(
+                ''.join(
+                    [
+                        '<li><a href="{}">{}</a></li>'.format(x, x)
+                        for x in SIG_SOURCES.keys()
+                    ]
+                )
+            )
+        )
