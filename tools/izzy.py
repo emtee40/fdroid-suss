2c01151458aedb5d38e989b685a8d0c08e834b55
linsui <2873532-linsui@users.noreply.gitlab.com>
2024-06-29 18:34:09 +0800

    izzy.py: only dump nonfree libs

--- tools/izzy.py
+++ tools/izzy.py
@@ -11,33 +11,41 @@ if __name__ == "__main__":
     outdir = Path('izzy')
     os.makedirs(outdir, exist_ok=True)
 
-    for libfileurl in [
-        'https://gitlab.com/IzzyOnDroid/repo/-/raw/master/lib/libinfo.jsonl',
-        'https://gitlab.com/IzzyOnDroid/repo/-/raw/master/lib/libsmali.jsonl',
-    ]:
-        cnt = 0
-        print('processing \'', libfileurl, '\' ...', end=' ')
-        with urllib.request.urlopen(libfileurl) as f:
-            for line in f:
-                data = json.loads(line)
-                if not data.get('anti') and data.get('license'):
-                    continue
-                name = data['id'].strip('/').replace('/', '.')
-                out = dict()
-                for k, v in data.items():
-                    if not v:
-                        continue
-                    if k == 'anti':
-                        out['anti_features'] = v
-                    elif k == 'details':
-                        out['description'] = v
-                    elif k == 'id':
-                        out['code_signatures'] = [v.strip('/').replace('/', '\\.')]
-                    else:
-                        out[k] = v
-                with open(outdir / (id + '.yml'), 'w', encoding='utf8') as fout:
-                    yaml = ruamel.yaml.YAML()
-                    yaml.indent(mapping=2, sequence=4, offset=2)
-                    yaml.dump(out, stream=fout)
-                cnt += 1
-        print('updated', cnt, 'files ... OK')
+    libinfourl = 'https://gitlab.com/IzzyOnDroid/repo/-/raw/master/lib/libinfo.jsonl'
+    libsmaliurl = 'https://gitlab.com/IzzyOnDroid/repo/-/raw/master/lib/libsmali.jsonl'
+    libs = {}
+    cnt = 0
+
+    with urllib.request.urlopen(libinfourl) as f:
+        for line in f:
+            data = json.loads(line)
+            if data.get('license') == "Proprietary":
+                libs[data["id"]] = data
+
+    with urllib.request.urlopen(libsmaliurl) as f:
+        for line in f:
+            smali = json.loads(line)
+            info = libs.get(smali['id'])
+            if not info:
+                continue
+            id = smali['id'].strip('/').replace('/', '.')
+
+            out = {
+                'license': 'NonFree',
+                'anti_features': info.get('anti'),
+                'description': info.get('details'),
+                'code_signatures': [smali.get('path').strip('/')],
+                'name': info.get('name'),
+                'documentation': info.get('url'),
+            }
+
+            out = {k: v for k, v in out.items() if v and v != [None]}
+
+            with open(outdir / (id + '.yml'), 'w', encoding='utf8') as fout:
+                yaml = ruamel.yaml.YAML()
+                yaml.indent(mapping=2, sequence=4, offset=2)
+                yaml.dump(out, stream=fout)
+
+            cnt += 1
+
+    print('updated', cnt, 'files ... OK')
